d76b065918e04b162d10627391484e56d6c15016
kie <179924858+kiieee@users.noreply.github.com>
2024-09-10 13:46:19 +0100

    added char code and added basic style

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const keyInfoDiv = document.getElementById('keyInfo');
 document.addEventListener('keydown', function(event) {
 const keyName = event.key;
 const keyDetails = event.code;
+const charCode = keyName.charCodeAt(0);
 
-keyInfoDiv.textContent = `Key: ${keyName} | Key Details: ${keyDetails}`;
+keyInfoDiv.textContent = `Key: ${keyName} | Key Code: ${keyDetails} | Character Code: ${charCode}`;
 });
